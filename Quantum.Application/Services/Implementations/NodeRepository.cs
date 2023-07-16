192c0e9f5b7caa6efa5d62c5317344313f5eff02
trueberryless <felix@schneider-it.at>
2023-07-16 11:11:01 +0200

    Card Site

--- Quantum.Application/Services/Implementations/NodeRepository.cs
+++ Quantum.Application/Services/Implementations/NodeRepository.cs
@@ -1,4 +1,5 @@
-﻿using Quantum.Application.Services.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using Quantum.Application.Services.Interfaces;
 using Quantum.Domain.Configuration;
 using Quantum.Domain.Entities;
 
@@ -8,5 +9,24 @@ public class NodeRepository : ARepository<Node>, INodeRepository
 {
     public NodeRepository(ModelDbContext context) : base(context)
     {
+    }
+
+    public async Task<Node> IncludeChildren(Node currentNode, int levelAmount)
+    {
+        if (levelAmount <= 0)
+            return currentNode;
+
+        // Including immediate children
+        currentNode = await Context.Nodes
+            .Include(n => n.Children)
+            .FirstAsync(n => n.Id == currentNode.Id);
+
+        // Including children's children recursively
+        foreach (var child in currentNode.Children)
+        {
+            await IncludeChildren(child, levelAmount - 1);
+        }
+
+        return currentNode;
     }
 }
